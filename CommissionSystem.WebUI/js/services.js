b0f8fc144f951f0733f0100b6a1216601badb83a
wfsiew <siewwingfei@hotmail.com>
2014-07-16 23:04:58 +0800

    j

---
+++ CommissionSystem.WebUI/js/services.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('noCacheInterceptor');
+}])
+.factory('noCacheInterceptor', function () {
+    return {
+        request: function (config) {
+            if (config.method == 'GET' && (config.url.indexOf('ngview/') < 0 && config.url.indexOf('.html') < 0)) {
+                var sep = config.url.indexOf('?') === -1 ? '?' : '&';
+                config.url = config.url + sep + 'noCache=' + new Date().getTime();
+            }
+
+            return config;
+        }
+    };
+});
